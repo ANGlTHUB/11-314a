69fc787c20de564681dda30a6910996c7eee898c
AN <73964712+ANGlTHUB@users.noreply.github.com>
2024-03-01 08:11:59 +0300

    Update code.py

---
+++ Week_project_2_Old/code.py
@@ -0,0 +1,26 @@
+def countPairs(nums, target):
+    nums.sort()  
+    count = 0  
+    n = len(nums)
+    # Initialize pointers
+    lft = 0  
+    rgt = n - 1  
+    
+    while lft < rgt:  
+        if nums[lft] + nums[rgt] < target:
+            count += (rgt - lft)  # Increment count by pairs numbers
+            lft += 1
+        else:
+            rgt -= 1
+    
+    return count  
+
+# Exemples arrays
+
+
+nums2 = [-6, 2, 5, -2, -7, -1, 3]
+target2 = -2
+
+#  testing
+output2 = countPairs(nums2, target2)
+print("nums2:", output2)
